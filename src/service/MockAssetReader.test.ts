5c7311c08270e66a8d8c9bb3f116d9f28bd7dc51
Hyunsoo-dev <jing07161@gmail.com>
2021-04-02 19:12:56 +0900

    [wip] Mock Data Reader Implementation

--- src/service/MockAssetReader.test.ts
+++ src/service/MockAssetReader.test.ts
@@ -1,10 +1,31 @@
-import Process from "./MockAssetReader";
+import {
+  Process,
+  descendingProcess,
+  ascendingProcess,
+  toInvestmentList,
+  toLiveList,
+} from './MockAssetReader';
 
+describe('test read mock json file', () => {
+  it('test 1', () => {
+    expect(Process()).toEqual(Process());
+  });
 
-describe("test read mock json file", () => {
+  it('test 2 #Sort list by descending order', () => {
+    descendingProcess();
+  });
 
-    it("test 1", () => {
-        Process();
-    })
+  it('test 3 #Sort list by ascending order', () => {
+    ascendingProcess();
+  });
 
+  it('test 4 #List of investment purpose', () => {
+    toInvestmentList();
+  });
+
+  it('test 5 #List of live purpose', () => {
+    toLiveList();
+  });
+
+  it('test 6 #List of investment purpose and sort list by descending order', () => {});
 });
--- src/service/MockAssetReader.ts
+++ src/service/MockAssetReader.ts
@@ -1,16 +1,98 @@
+import files from '../asset/mock_data.json';
+import Asset from '../model/Asset';
 
-import file from "../asset/mock_data.json";
-import Asset from "../model/Asset";
+//////////? key값이 "buildingInformation" 인 경우 해당 객체안에 값을 다시 Map에 저장하는 메소드
 
-function Process(): void {
-    let map = new Map<string, any>();
-    for (var value in file) {
-        map.set(value, file[value]);
-    }
-
-    let asset = Asset.fromMap(map);
-    console.log(asset);
+function newProcess(buildingInformation: any): Map<string, any> {
+  let map = new Map<string, any>();
+  for (let value in buildingInformation) {
+    map.set(value, buildingInformation[value]);
+  }
+  return map;
 }
 
+export function Process(): void {
+  let map = new Map<string, any>();
+  let asset: Asset;
+  let assetList: Asset[] = [];
+  for (let file of files) {
+    // 배열 순회
+    for (let [key, value] of Object.entries(file)) {
+      // 객체 순회
+      if (key === 'buildingInformation') {
+        let buildingInfoMap = newProcess(file[key]);
+        map.set(key, buildingInfoMap);
+      } else {
+        map.set(key, value);
+      }
+    }
 
-export default Process;
+    assetList.push(Asset.fromMap(map));
+  }
+  //! console.log(assetList);
+}
+
+// 내림차순 정렬
+export function descendingProcess(param: any[] = files): void {
+  let map = new Map<string, any>();
+  let asset: Asset;
+  let assetList: Asset[] = [];
+
+  for (let file of param) {
+    // 배열 순회
+    for (let [key, value] of Object.entries(file)) {
+      // 객체 순회
+      if (key === 'buildingInformation') {
+        let buildingInfoMap = newProcess(file[key]);
+        map.set(key, buildingInfoMap);
+      } else {
+        map.set(key, value);
+      }
+    }
+
+    assetList.push(Asset.fromMap(map));
+    assetList.sort((a, b) => b.price - a.price);
+  }
+  //! console.log(assetList);
+}
+
+// 오름차순 정렬
+export function ascendingProcess(param: any[] = files): void {
+  let map = new Map<string, any>();
+  let asset: Asset;
+  let assetList: Asset[] = [];
+
+  for (let file of param) {
+    // 배열 순회
+    for (let [key, value] of Object.entries(file)) {
+      // 객체 순회
+      if (key === 'buildingInformation') {
+        let buildingInfoMap = newProcess(file[key]);
+        map.set(key, buildingInfoMap);
+      } else {
+        map.set(key, value);
+      }
+    }
+
+    assetList.push(Asset.fromMap(map));
+    assetList.sort((a, b) => a.price - b.price);
+  }
+  //! console.log(assetList);
+}
+
+////////////? 투자 목적이면 true, 거주 목적이면 false
+
+// 투자 목적 리스트 가져오는 함수
+export function toInvestmentList(): any {
+  let investmentList = files.filter((file) => file.investment);
+
+  //! console.log(investmentList);
+  //! return investmentList;
+}
+
+// 거주 목적 리스트 가져오는 함수
+export function toLiveList(): void {
+  let liveList = files.filter((file) => !file.investment);
+
+  //! console.log(liveList);
+}
