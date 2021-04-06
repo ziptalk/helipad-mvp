31427a56ac467d8b52578d431bf8cc94f5acb7c7
boohyunsik <bhs9194@nate.com>
2021-04-06 14:10:52 +0900

    firebase setting

---
+++ src/shared/Firebase.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+
+// Initialize Firebase
+firebase.initializeApp(
+    {
+        apiKey: "AIzaSyBVFAr_6d_n1MJehUvQ9ZvgRr-2JA4jLMs",
+        authDomain: "helipad-mvp.firebaseapp.com",
+        projectId: "helipad-mvp",
+        storageBucket: "helipad-mvp.appspot.com",
+        messagingSenderId: "238198528208",
+        appId: "1:238198528208:web:66f62e144715657104a6d5",
+        measurementId: "G-2MJTY29YZD"
+    }
+);
+
+const store = firebase.firestore().collection("assets");
+
+export {firebase, store};
