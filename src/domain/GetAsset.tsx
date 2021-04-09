7c181dc7c84aa984fc8ab78497223f2d94b477e5
boohyunsik <bhs9194@nate.com>
2021-04-09 20:59:19 +0900

    divide duplicated code to method

--- src/domain/GetAsset.tsx
+++ src/domain/GetAsset.tsx
@@ -1,29 +1,30 @@
 import Asset, {COLUMNS} from "../model/Asset";
 import FirebaseService from "../service/FirebaseService";
+import firebase from "firebase";
+import DocumentData = firebase.firestore.DocumentData;
 
 export default class GetAsset {
     async getAssetList(): Promise<Asset[]> {
         let assets = await new FirebaseService().getAssetList();
         return assets.map((asset) => {
-            let data = asset.data();
-            let assetMap = new Map(Object.entries(data));
-            let buildingInfoObject = data.buildingInformation;
-            assetMap.set(COLUMNS.BUILDING_INFORMATION, new Map(Object.entries(buildingInfoObject)));
-            return Asset.fromMap(assetMap);
+            return this.parseAsset(asset.data());
         });
     }
 
     async getAsset(id: string): Promise<Asset> {
         let asset = await new FirebaseService().getAsset(id);
         if (asset.docs.length === 1) {
-            let data = asset.docs[0].data();
-            let assetMap = new Map(Object.entries(data));
-            let buildingInfoObject = data.buildingInformation;
-            assetMap.set(COLUMNS.BUILDING_INFORMATION, new Map(Object.entries(buildingInfoObject)));
-            return Asset.fromMap(assetMap);
+            return this.parseAsset(asset.docs[0].data());
         } else {
             console.log("TODO : throw error and handle in UI");
             return Asset.emptyAsset();
         }
+    }
+
+    private parseAsset(asset: DocumentData): Asset {
+        let assetMap = new Map(Object.entries(asset));
+        let buildingInfoObject = asset.buildingInformation;
+        assetMap.set(COLUMNS.BUILDING_INFORMATION, new Map(Object.entries(buildingInfoObject)));
+        return Asset.fromMap(assetMap);
     }
 }
--- src/service/FirebaseService.ts
+++ src/service/FirebaseService.ts
@@ -9,44 +9,4 @@ export default class FirebaseService {
     async getAsset(id: string) {
         return await store.where("id", "==", id).get();
     }
-
-    // async saveAssetList(list: Asset[]) {
-    //     list.forEach((asset) => {
-    //         store.add({
-    //             "forInvestment": asset.forInvestment,
-    //             "price": asset.price,
-    //             "status": asset.status,
-    //             "daysOnMarket": asset.daysOnMarket,
-    //             "taxPerMonth": asset.taxPerMonth,
-    //             "commonChargePerMonth": asset.commonChargePerMonth,
-    //             "minDownPymt": asset.minDownPymt,
-    //             "compassType": asset.compassType,
-    //             "mlsType": asset.mlsType,
-    //             "expectedMonthlyPayment": asset.expectedMonthlyPayment,
-    //             "expectedMonthlyRent": asset.expectedMonthlyRent,
-    //             "information": asset.information,
-    //             "amenities": asset.amenities,
-    //             "buildingInformation": {
-    //                 "thumbnail": asset.buildingInformation.thumbnailUrl,
-    //                 "address": asset.buildingInformation.address,
-    //                 "street": asset.buildingInformation.street,
-    //                 "nBedrooms": asset.buildingInformation.nBedrooms,
-    //                 "nBathrooms": asset.buildingInformation.nBathrooms,
-    //                 "nRooms": asset.buildingInformation.nRooms,
-    //                 "square": asset.buildingInformation.square,
-    //                 "country": asset.buildingInformation.country,
-    //                 "buildingName": asset.buildingInformation.buildingName,
-    //                 "floor": asset.buildingInformation.floor,
-    //                 "stories": asset.buildingInformation.stories,
-    //                 "residences": asset.buildingInformation.residences,
-    //                 "petPolicy": asset.buildingInformation.petPolicy,
-    //                 "yearBuilt": asset.buildingInformation.yearBuilt,
-    //                 "buildingAge": asset.buildingInformation.buildingAge,
-    //                 "buildingType": asset.buildingInformation.buildingType
-    //             }
-    //         }).then(value => {
-    //             console.log("add complete! ", value);
-    //         });
-    //     });
-    // }
 }
