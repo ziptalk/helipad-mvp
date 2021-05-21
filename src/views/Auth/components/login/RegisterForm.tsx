74916baf88e36c8d49f7b4fb31e248583311fd62
Hyunsoo-dev <jing07161@gmail.com>
2021-05-21 16:16:06 +0900

    [wip] Refactoring

--- src/views/Auth/components/login/RegisterForm.tsx
+++ src/views/Auth/components/login/RegisterForm.tsx
@@ -1,83 +1,210 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router';
-import styled from 'styled-components';
-import { InputField, InputType } from './InputField';
-import { CheckboxField, CheckBoxType } from './CheckBoxField';
-let mockData = [
-  {
-    title: 'How did you discover Helipad',
-    name: 'discover',
-    contents: [
-      'Search engine (Google, Naver, etc.)',
-      'Recommended by friend or colleague',
-      'Social media',
-      'Blog or publication',
-      'Other',
-    ],
-  },
-  {
-    title:
-      'Why are you interested in a home in the U.S.? (check all that apply)',
-    name: 'interested',
-    contents: [
-      'Investment',
-      'Rental Income Opportunity',
-      'Second Home',
-      'Relocating to U.S',
-      "Child's U.S. Education",
-    ],
-  },
-  {
-    title: 'What is your ideal price point? (check all that apply)',
-    name: 'idealPrice',
-    contents: [
-      'Under $500K',
-      'US $500K - $1mm',
-      'US $1mm - $2.5m',
-      '$2.5m - $5.0m',
-      '$5.0m - 10m',
-      '$10m + ',
-    ],
-  },
-  {
-    title: 'What is your preferred area? (check all that apply)',
-    name: 'preferredArea',
-    contents: [
-      'CA - Los Angeles',
-      'CA - Orange County',
-      'CA - San Diego',
-      'CA - San Francisco',
-      'NV - Las Vegas',
-      'New York',
-      'New Jersey',
-      'Others (Fill in)',
-    ],
-  },
-];
-type mockDataProps = {
-  title: string;
-  name: string;
-  contents: string[];
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import styled from "styled-components";
+import { InputField, InputType } from "./InputField";
+import { CheckboxField, CheckBoxType } from "./RegisterFormField";
+
+let mockData = {
+  baseInfo: [
+    {
+      id: "1",
+      type: CheckBoxType.TEXT,
+      title: "1. First Name",
+      name: "firstName",
+    },
+    {
+      id: "1",
+      type: CheckBoxType.TEXT,
+      title: "2. Last name",
+      name: "lastName",
+    },
+    {
+      id: "1",
+      type: CheckBoxType.TEXT,
+      title: "3. Email",
+      name: "email",
+    },
+    {
+      id: "1",
+      type: CheckBoxType.TEXT,
+      title: "4. KaKao ID",
+      name: "kakaoId",
+    },
+  ],
+  discover: [
+    {
+      id: "2",
+      type: CheckBoxType.CHECKBOX,
+      title: "Search engine (Google, Naver, etc.)",
+      name: "discover",
+      onChange: "handleDiscoverPath",
+    },
+    {
+      id: "2",
+      type: CheckBoxType.CHECKBOX,
+      title: "Recommended by friend or colleague",
+      name: "discover",
+    },
+    {
+      id: "2",
+      type: CheckBoxType.CHECKBOX,
+      title: "Social media",
+      name: "discover",
+    },
+    {
+      id: "2",
+      type: CheckBoxType.CHECKBOX,
+      title: "Blog or publication",
+      name: "discover",
+    },
+    {
+      id: "2",
+      type: CheckBoxType.TEXT,
+      title: "Other",
+      name: "discover",
+    },
+  ],
+  interested: [
+    {
+      id: "3",
+      type: CheckBoxType.CHECKBOX,
+      title: "Investment",
+      name: "interested",
+    },
+    {
+      id: "3",
+      type: CheckBoxType.CHECKBOX,
+      title: "Rental Income Opportunity",
+      name: "interested",
+    },
+    {
+      id: "3",
+      type: CheckBoxType.CHECKBOX,
+      title: "Second Home",
+      name: "interested",
+    },
+    {
+      id: "3",
+      type: CheckBoxType.CHECKBOX,
+      title: "Relocating to U.S.",
+      name: "interested",
+    },
+    {
+      id: "3",
+      type: CheckBoxType.CHECKBOX,
+      title: "Child’s U.S. Education",
+      name: "interested",
+    },
+  ],
+  idealPrice: [
+    {
+      id: "4",
+      type: CheckBoxType.CHECKBOX,
+      title: "Under $500K",
+      name: "idealPrice",
+    },
+    {
+      id: "4",
+      type: CheckBoxType.CHECKBOX,
+      title: "US $500K - $1mm",
+      name: "idealPrice",
+    },
+    {
+      id: "4",
+      type: CheckBoxType.CHECKBOX,
+      title: "US $1mm-$2.5m",
+      name: "idealPrice",
+    },
+    {
+      id: "4",
+      type: CheckBoxType.CHECKBOX,
+      title: "$2.5m-$5.0m",
+      name: "idealPrice",
+    },
+    {
+      id: "4",
+      type: CheckBoxType.CHECKBOX,
+      title: "$5.0m-10m",
+      name: "idealPrice",
+    },
+  ],
+  preferredArea: [
+    {
+      id: "5",
+      type: CheckBoxType.CHECKBOX,
+      title: "CA – Los Angeles",
+      name: "preferredArea",
+    },
+    {
+      id: "5",
+      type: CheckBoxType.CHECKBOX,
+      title: "CA – Orange County",
+      name: "preferredArea",
+    },
+    {
+      id: "5",
+      type: CheckBoxType.CHECKBOX,
+      title: "CA – San Diego",
+      name: "preferredArea",
+    },
+    {
+      id: "5",
+      type: CheckBoxType.CHECKBOX,
+      title: "CA – San Francisco",
+      name: "preferredArea",
+    },
+    {
+      id: "5",
+      type: CheckBoxType.CHECKBOX,
+      title: "NV – Las Vegas",
+      name: "preferredArea",
+    },
+    {
+      id: "5",
+      type: CheckBoxType.CHECKBOX,
+      title: "New York",
+      name: "preferredArea",
+    },
+    {
+      id: "5",
+      type: CheckBoxType.CHECKBOX,
+      title: "New Jersey",
+      name: "preferredArea",
+    },
+    {
+      id: "5",
+      type: CheckBoxType.TEXT,
+      title: "Others (Fill in)",
+      name: "preferredArea",
+    },
+  ],
 };
-type SelectProps = {
-  discover: string[];
-  interested: string[];
-  idealPrice: string[];
-  preferredArea: string[];
+
+type BaseInfoProps = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  kakaoId: string;
 };
+
 const RegisterForm = () => {
   const history = useHistory();
-  const [firstName, setFirstName] = useState<string>('');
-  const [lastName, setLastName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [kakaoId, setKakaoId] = useState<string>('');
+  const [baseInfo, setBaseInfo] = useState<BaseInfoProps>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    kakaoId: "",
+  });
   const [discoverPath, setDiscoverPath] = useState<string[]>([]);
   const [interested, setInterested] = useState<string[]>([]);
   const [idealPrice, setIdealPrice] = useState<string[]>([]);
   const [preferredArea, setPreferredArea] = useState<string[]>([]);
-  const [discoverPathOther, setDiscoverPathOther] = useState<string>('');
-  const [preferredAreaOther, setPreferredAreaOther] = useState<string>('');
+  const [discoverPathOther, setDiscoverPathOther] = useState<string>("");
+  const [preferredAreaOther, setPreferredAreaOther] = useState<string>("");
 
+  const handleBaseInfo = (name: string, value: string) => {
+    setBaseInfo({ ...baseInfo, [name]: value });
+  };
   const handleDiscoverPath = (checked: boolean, value: string) => {
     if (checked) {
       setDiscoverPath([...discoverPath, value]);
@@ -106,22 +233,23 @@ const RegisterForm = () => {
       setPreferredArea(preferredArea.filter((ele) => ele !== value));
     }
   };
-
+  const handleDiscoverPathOther = (name: string, value: string) => {
+    setDiscoverPathOther(value);
+  };
+  const handlePreferredAreaOther = (name: string, value: string) => {
+    setPreferredAreaOther(value);
+  };
   const onTryRegister = () => {
     let result = {
-      firstName,
-      lastName,
-      email,
-      kakaoId,
+      ...baseInfo,
       discoverPath: [...discoverPath, discoverPathOther],
       interested,
       idealPrice,
       preferredArea: [...preferredArea, preferredAreaOther],
     };
-    console.log('result data :', result);
+    console.log("result data :", result);
     history.push({
-      pathname: '/auth/login',
-      state: { email: email },
+      pathname: "/auth/login",
     });
   };
 
@@ -130,240 +258,90 @@ const RegisterForm = () => {
       <Title>Register</Title>
       <Form>
         <Divider />
-        <CheckboxField
-          type={CheckBoxType.TEXT}
-          title="1. First name"
-          name="first name"
-          onChange={(name: string) => setFirstName(name)}
-        />
+        {mockData.baseInfo.map((data) => (
+          <CheckboxField
+            type={data.type}
+            title={data.title}
+            name={data.name}
+            onChange={handleBaseInfo}
+          />
+        ))}
 
-        <CheckboxField
-          type={CheckBoxType.TEXT}
-          title="2. Last name"
-          name="first name"
-          onChange={(name: string) => setLastName(name)}
-        />
-
-        <CheckboxField
-          type={CheckBoxType.EMAIL}
-          title="3. Email"
-          name="first name"
-          onChange={(name: string) => setEmail(name)}
-        />
-
-        <CheckboxField
-          type={CheckBoxType.TEXT}
-          title="4. Kakao ID"
-          name="first name"
-          onChange={(name: string) => setKakaoId(name)}
-        />
-
-        <Label>5. How did you discover Helipad?</Label>
+        <SubTitle>5. How did you discover Helipad?</SubTitle>
         <Ol type="a">
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="Search engine (Google, Naver, etc.)"
-              name="discover"
-              onChange={handleDiscoverPath}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="Recommended by friend or colleague"
-              name="discover"
-              onChange={handleDiscoverPath}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="Social media"
-              name="discover"
-              onChange={handleDiscoverPath}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="Blog or publication"
-              name="discover"
-              onChange={handleDiscoverPath}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.TEXT}
-              title="Other"
-              name="discover"
-              onChange={(value: string) => setDiscoverPathOther(value)}
-            />
-          </li>
+          {mockData.discover.map((data) => (
+            <li>
+              {data.type === CheckBoxType.CHECKBOX ? (
+                <CheckboxField
+                  type={data.type}
+                  title={data.title}
+                  name={data.name}
+                  onChange={handleDiscoverPath}
+                />
+              ) : (
+                <CheckboxField
+                  type={data.type}
+                  title={data.title}
+                  name={data.name}
+                  onChange={handleDiscoverPathOther}
+                />
+              )}
+            </li>
+          ))}
         </Ol>
-        <Label>
+
+        <SubTitle>
           6. Why are you interested in a home in the U.S.? (check all that
           apply)
-        </Label>
+        </SubTitle>
         <Ol type="a">
-          <li>
+          {mockData.interested.map((data) => (
             <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="Investment"
-              name="interested"
+              type={data.type}
+              title={data.title}
+              name={data.name}
               onChange={handleInterested}
             />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="Rental Income Opportunity"
-              name="interested"
-              onChange={handleInterested}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="Second Home"
-              name="interested"
-              onChange={handleInterested}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="Relocating to U.S."
-              name="interested"
-              onChange={handleInterested}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="Child’s U.S. Education"
-              name="interested"
-              onChange={handleInterested}
-            />
-          </li>
+          ))}
         </Ol>
-        <Label>7. What is your ideal price point? (check all that apply)</Label>
+
+        <SubTitle>
+          7. What is your ideal price point? (check all that apply)
+        </SubTitle>
         <Ol type="a">
-          <li>
+          {mockData.idealPrice.map((data) => (
             <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="Under $500K"
-              name="idealPrice"
+              type={data.type}
+              title={data.title}
+              name={data.name}
               onChange={handleIdealPrice}
             />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="US $500K - $1mm"
-              name="idealPrice"
-              onChange={handleIdealPrice}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="US $1mm-$2.5m"
-              name="idealPrice"
-              onChange={handleIdealPrice}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="$2.5m-$5.0m"
-              name="idealPrice"
-              onChange={handleIdealPrice}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="$5.0m-10m"
-              name="idealPrice"
-              onChange={handleIdealPrice}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="$10m +"
-              name="idealPrice"
-              onChange={handleIdealPrice}
-            />
-          </li>
+          ))}
         </Ol>
-        <Label>8. What is your preferred area? (check all that apply)</Label>
+
+        <SubTitle>
+          8. What is your preferred area? (check all that apply)
+        </SubTitle>
         <Ol type="a">
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="CA – Los Angeles"
-              name="preferredArea"
-              onChange={handlePreferredArea}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="CA – Orange County"
-              name="preferredArea"
-              onChange={handlePreferredArea}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="CA – San Diego"
-              name="preferredArea"
-              onChange={handlePreferredArea}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="CA – San Francisco"
-              name="preferredArea"
-              onChange={handlePreferredArea}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="NV – Las Vegas"
-              name="preferredArea"
-              onChange={handlePreferredArea}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="New York"
-              name="preferredArea"
-              onChange={handlePreferredArea}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.CHECKBOX}
-              title="New Jersey"
-              name="preferredArea"
-              onChange={handlePreferredArea}
-            />
-          </li>
-          <li>
-            <CheckboxField
-              type={CheckBoxType.TEXT}
-              title="Others (Fill in)"
-              name="preferredArea"
-              onChange={(value: string) => setPreferredAreaOther(value)}
-            />
-          </li>
+          {mockData.preferredArea.map((data) => (
+            <li>
+              {data.type === CheckBoxType.CHECKBOX ? (
+                <CheckboxField
+                  type={data.type}
+                  title={data.title}
+                  name={data.name}
+                  onChange={handlePreferredArea}
+                />
+              ) : (
+                <CheckboxField
+                  type={data.type}
+                  title={data.title}
+                  name={data.name}
+                  onChange={handlePreferredAreaOther}
+                />
+              )}
+            </li>
+          ))}
         </Ol>
       </Form>
       <Button onClick={onTryRegister}>Register</Button>
@@ -382,7 +360,7 @@ const Title = styled.div`
   border-bottom: 2px solid lightgray;
   margin-bottom: 20px;
 `;
-const Label = styled.label``;
+const SubTitle = styled.div``;
 const Form = styled.form`
   margin-bottom: 20px;
 `;
