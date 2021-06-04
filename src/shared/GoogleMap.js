760d920e9652ff17dee258126711b03aeaa7a3dd
hyunju <dlguswn7768@gmail.com>
2021-06-04 19:17:14 +0900

    prop으로 전달받은 빌딩 데이터들 전부 맵에 올바르게 마커 표시하기

--- src/shared/GoogleMap.js
+++ src/shared/GoogleMap.js
@@ -19,6 +19,16 @@ export class MapContainer extends React.Component {
             address : "66 North 1st Street, Unit 3B, Williamsburg, Brooklyn, NY 11249",
             latState : 0,
             lngState : 30,
+            assetState : [
+                {
+                    assetId: 0,
+                    assetAddress : "",
+                    assetLat : 0,
+                    assetLng : 31,
+                    assetLabel: "1,234,567,890,000"
+
+                },
+            ],
             fetchingServices: true,
         }
         this.fetchServices = this.fetchServices.bind(this);
@@ -28,8 +38,17 @@ export class MapContainer extends React.Component {
         this.fetchServices();
      }
 
-    fetchServices(){
-        const GoogleMapLocate = async (currentAddr) => {
+    async fetchServices(){
+        let assetStateList = [
+            {
+                assetId: 0,
+                assetAddress : "",
+                assetLat : 0,
+                assetLng : 31,
+                assetLabel: "1,234,567,890,000"
+            },
+        ]
+        const GoogleMapLocate = async (currentAddr, index) => {
 
             Geocode.setApiKey('AIzaSyAHHYSWgQGMPHXYRqCMMUSlxTvqrDepyeA')
             Geocode.setLanguage('en')
@@ -46,34 +65,90 @@ export class MapContainer extends React.Component {
                     lngState: lng,
                     fetchingServices: false,
                 });
+
+                // this.state.assetState.map(data =>
+                //     data.assetId === index ?
+                //     ({ ...data, assetLat: lat, assetLng: lng})
+                //     : data
+                // )
+
+                // assetStateList.map(data =>
+                //     data.assetId === index ?
+                //     ({ ...data, assetLat: lat, assetLng: lng})
+                //     : data
+                // )
+
+                assetStateList[index].assetLat = lat;
+                assetStateList[index].assetLng= lng;
+
                 return {lat, lng}
               })
               .catch(err => console.log(err))
           }
 
-        GoogleMapLocate(this.state.address)
+        GoogleMapLocate(this.state.address, 0)
+
+        const { data } = this.props
+        // const { assetState } = this.state.assetState
+
+        let id = 1
+
+        // console.log(data)
+
+        await  data.map(function(asset){
+            // console.log(asset.buildingInformation.address)
+
+            // this.setState({
+            //     assetState : this.state.assetState.concat({ 
+            //         assetId: id,
+            //         assetAddress: asset.buildingInformation.address,
+            //     })
+            // });
+            assetStateList = assetStateList.concat({
+                assetId: id,
+                assetAddress: asset.buildingInformation.address,
+                assetLabel: (asset.price*1000).toString()
+                .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+            })
+            id++;
+            // console.log(id)
+        })
+
+        await  data.map((asset, index) => GoogleMapLocate(asset.buildingInformation.address, index + 1))
+        
+        // console.log(assetStateList)
+
+        await this.setState({
+            assetState : assetStateList
+        })
+        // console.log("hererer")
+        // console.log(this.state.assetState)
     }
 
     render() {
-        const { data } = this.props
-        console.log(data)
-        data.map((asset) => console.log(asset.buildingInformation.address))
-
         const mapStyles = {
             width: 'calc(100vw - 512px)',
             height: '100vh'
         };
 
+        // console.log(this.state.assetState)
+
         return (
             <Container>
-                <div>{this.state.latState}</div>
+                {/* <div>{this.state.latState}</div>
                 <div>{this.state.lngState}</div>
+                <div>
+                    {
+                    this.state.assetState.map((asset) => asset.assetAddress)
+                    }
+                </div> */}
+
                 {/* <div>{
                 data.map((asset) => asset)
                 }</div> */}
                 <Map
                     google={this.props.google}
-                    zoom={10}
+                    zoom={14}
                     style={mapStyles}
                     initialCenter={{
                         lat: 40.7164377,
@@ -87,19 +162,22 @@ export class MapContainer extends React.Component {
                         name="My Marker"
                         color="blue"
                     /> */}
-                    <Marker key="marker_1"
-                     icon={{
-                        url: 'https://www.asianpaints.com/content/dam/asian_paints/colours/swatches/7661.png.transform/cc-width-720-height-540/image.png',
-                        anchor: new window.google.maps.Point(17, 46),
-                        scaledSize: new window.google.maps.Size(150, 37)
-                    }}
-                    position={{
-                        lat: this.state.latState,
-                        lng: this.state.lngState
-                    }}
-                    label="1,234,567,809,000"
-                    >
-                    </Marker>
+                    {this.state.assetState.map(asset => 
+                        <Marker key="marker_1"
+                        icon={{
+                           url: 'https://www.asianpaints.com/content/dam/asian_paints/colours/swatches/7661.png.transform/cc-width-720-height-540/image.png',
+                           anchor: new window.google.maps.Point(17, 46),
+                           scaledSize: new window.google.maps.Size(150, 37)
+                       }}
+                       position={{
+                           lat: asset.assetLat,
+                           lng: asset.assetLng
+                       }}
+                       label={String(asset.assetLabel)}
+                       >
+                       </Marker>
+                    )}
+                    
                 </Map>
             </Container>
         );
